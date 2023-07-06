e435fe893b369b47df4f2cdba0c7396296f4ca61
dichev <g.dichev@gmail.com>
2023-07-06 14:53:10 -0700

    Iterate through nested params

--- src/models/optimizers.py
+++ src/models/optimizers.py
@@ -8,11 +8,23 @@ class Optimizer:
 
     @torch.no_grad()
     def step(self):
-        for param in self._params:
+        for param in nested(self._params):
             param -= self.lr * param.grad
             
         return self
 
     def zero_grad(self):
-        for param in self._params:
+        for param in nested(self._params):
             param.grad.zero_()
+
+
+def nested(t):
+    for i in t:
+        if isinstance(i, tuple):
+            yield from nested(i)
+        else:
+            yield i
+
+# tup = ((1, 2, 3), (4, 5, (6, 7)), 8, 9)
+# for elem in nested(tup):
+#     print(elem)
--- src/models/shallow_models.py
+++ src/models/shallow_models.py
@@ -4,10 +4,10 @@ from functions.activations import sigmoid, softmax
 
 
 class Linear:
-    def __init__(self, input_size, output_size=1):
-        self.W = torch.rand(input_size, output_size, requires_grad=True)  # (D, C)
-        self.b = torch.rand(1, output_size, requires_grad=True)           # (1, C)
-        self.params = (self.W, self.b)
+    def __init__(self, input_size, output_size=1, device='cpu'):
+        self.W = torch.rand(input_size, output_size, device=device, requires_grad=True)  # (D, C)
+        self.b = torch.rand(1, output_size, device=device, requires_grad=True)           # (1, C)
+        self.params = (self.W, self.b) # todo: consider extending torch.nn.Module
 
     def forward(self, X):
         z = X @ self.W + self.b    # (N, D)x(D, C) + (1, C)  --> (N, C)
