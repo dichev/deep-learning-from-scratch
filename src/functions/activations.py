26f0626fb8fcfa618a6b616f9f4c89ad2a113056
dichev <g.dichev@gmail.com>
2023-07-14 14:58:24 -0700

    Numerically stable softmax (implement the LogSumExp trick)

--- src/functions/activations.py
+++ src/functions/activations.py
@@ -9,13 +9,26 @@ def unit_step(x):
 def sigmoid(x):
     return 1 / (1 + torch.exp(-x))
 
-def softmax(z):
-    e = torch.exp(z - z.max())
-    return e / e.sum(dim=-1, keepdim=True)
+def softmax(z, dim=-1):
+    """ Shift with the max value to avoid numerical overflows:
+    ->  softmax(z) =  e^{z_i} / sum e^z  * e^{-c}/e^{-c} = e^{z_i-c} / sum e^{z-c} = softmax(z-c)
+    """
+    e = torch.exp(z - z.max(dim, keepdim=True))
+    return e / e.sum(dim, keepdim=True)
 
+def log_softmax(z, dim=-1):
+    """ Division to subtraction & LogSumExp trick:
+    ->  ln(softmax(z)) = ln( e^{z_i} / sum e^z) = z_i - ln(sum e^z)
+    """
+    return z - log_sum_exp(z, dim)
 
-def log_softmax(z):
-    e = z.exp().sum(dim=-1, keepdim=True)
-    return z - e.log()  # that can be approximated as: z - max(z)
+def log_sum_exp(z, dim=-1):
+    """ LogSumExp trick for numerical stability:
+    ->  ln(sum e^z) = ln(sum e^z e^c e^{-c}) = c - ln(sum e^{z-c})
+    The best choice for c is the max value of z, because then the largest exponent will be 1:
+    ->  max(e^{x-max(x)}) = e^0 = 1
+    """
+    c = z.max(dim, keepdim=True)[0]
+    return c + torch.log(torch.exp(z-c).sum(dim, keepdim=True))
 
 
