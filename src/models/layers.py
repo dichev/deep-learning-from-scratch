0b26f74e791469127854ac9266ab74e2ca7be7e0
dichev <g.dichev@gmail.com>
2023-08-17 11:36:59 -0700

    Print model's structure and export its parameters to json

--- src/models/layers.py
+++ src/models/layers.py
@@ -1,25 +1,45 @@
+import json
 import torch
 from functions import init
 from models.parameters import Param
 
-
 class Module:
-    def parameters(self, named=True):
+
+    def parameters(self, named=True, prefix=''):
         for key, val in vars(self).items():
             if isinstance(val, Module):
-                yield from val.parameters(named)
+                yield from val.parameters(named, prefix=f'{key}.')
             elif hasattr(val, '_is_parameter') and getattr(val, '_is_parameter') is True:
-                yield (key, val) if named else val
+                yield (prefix + key, val) if named else val
+
+    def summary(self):
+        print(self)
+        for key, val in self.parameters():
+            print(f'\t{key}: {val.__class__.__name__}{list(val.size())}')
+
+    def export(self, filename='./logs/model.json'):
+        print('Export model to:', filename)
+        params = {key: value.tolist() for key, value in self.parameters(named=True)}
+        with open(filename, 'w') as f:
+            json.dump(params, f, indent=2)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}()'
+
 
 
 class Linear(Module):
     def __init__(self, input_size, output_size=1, device='cpu', weights_init=init.kaiming_normal_relu):
         self.weight = Param(input_size, output_size, init=weights_init, device=device, requires_grad=True)  # (D, C)
         self.bias = Param(1, output_size, init=init.zeros, device=device, requires_grad=True)  # (D, C)
+        self.input_size, self.output_size = input_size, output_size
 
     def forward(self, X):
         z = X @ self.weight + self.bias    # (N, D)x(D, C) + (1, C)  --> (N, C)
         return z
+
+    def __repr__(self):
+        return f'Linear({self.input_size}, {self.output_size})'
 
 
 class Embedding(Module):  # aka lookup table
@@ -36,6 +56,6 @@ class Embedding(Module):  # aka lookup table
         z = self.weight[indices]
         return z
 
-    def __str__(self):
+    def __repr__(self):
         return f'Embedding({self.input_size}, {self.output_size})'
 
