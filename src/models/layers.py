a3cf35b229bb628f439d816811d0d112ac1eb178
dichev <g.dichev@gmail.com>
2023-09-07 10:49:50 -0700

    Do not inherit Params, when performing math operations (instead return Tensor)

--- src/models/layers.py
+++ src/models/layers.py
@@ -9,7 +9,7 @@ class Module:
         for key, val in vars(self).items():
             if isinstance(val, Module):
                 yield from val.parameters(named, prefix=f'{key}.')
-            elif isinstance(val, Param):
+            elif type(val) is Param:  # don't use isinstance, because Param is a subclass of Tensor
                 yield (prefix + key, val) if named else val
 
     def modules(self, named=True, prefix=''):
--- src/models/parameters.py
+++ src/models/parameters.py
@@ -1,13 +1,12 @@
 import torch
+from torch._C import _disabled_torch_function_impl
 
 class Param(torch.Tensor):
     def __new__(cls, in_size, out_size, init, device=None, requires_grad=True):
         assert requires_grad, 'Parameters are expected to have gradients'
 
         data = init(in_size, out_size, device=device)
-        instance = torch.Tensor._make_subclass(cls, data)
-        # instance._is_parameter = True  # will be used to detect all module's Parameters
-        instance.requires_grad = True
+        instance = torch.Tensor._make_subclass(cls, data, requires_grad)
         return instance
 
     def __repr__(self, **kwargs):
@@ -16,6 +15,7 @@ class Param(torch.Tensor):
     def __str__(self):
         return torch.Tensor.__str__(self).replace('Param', 'tensor')
 
-
-
+    # __torch_function__ implementation wraps subclasses such that methods called on subclasses return a subclass instance instead of a torch.Tensor instance.
+    # However, this is not desired here, since we want to return a torch.Tensor instance when doing math operations on Param instances.
+    __torch_function__ = _disabled_torch_function_impl
 
