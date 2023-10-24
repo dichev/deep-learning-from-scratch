97f4ff1bf9dea0599e04bc05e93ad19d71c4c8b9
dichev <g.dichev@gmail.com>
2023-10-24 10:11:40 -0700

    Batch data without labels

--- src/models/training.py
+++ src/models/training.py
@@ -1,8 +1,11 @@
 import torch
 
-def batches(X, y, batch_size, shuffle=True, device=None):
-    n = len(y)
+def batches(X, y=None, batch_size=1024, shuffle=True, device=None):
+    n = len(X)
     indices = torch.randperm(n) if shuffle else torch.arange(n)
     for i in range(0, n, batch_size):
         batch = indices[i:i+batch_size]
-        yield X[batch].to(device), y[batch].to(device), len(batch)/n
+        if y is None:
+            yield X[batch].to(device), len(batch)/n
+        else:
+            yield X[batch].to(device), y[batch].to(device), len(batch)/n
