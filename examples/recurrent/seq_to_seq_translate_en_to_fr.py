ffcacc61dd44a8b0cae9a64ddfff1d8830900ddf
dichev <g.dichev@gmail.com>
2024-03-26 12:09:12 -0700

    Implement Sequence to Sequence model

---
+++ examples/recurrent/seq_to_seq_translate_en_to_fr.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import torch
+from torchvision.datasets.utils import download_and_extract_archive
+from tqdm import trange
+
+from models.recurrent_networks import Seq2Seq, Encoder, Decoder
+from lib.optimizers import Adam
+from lib.functions.losses import cross_entropy, accuracy
+from lib.regularizers import grad_clip_norm_
+from lib.training import batches
+from preprocessing.text import clean_text, TextVocabulary
+
+# training settings
+EPOCHS = 100
+BATCH_SIZE = 1024
+LEARN_RATE = 0.02
+DEVICE = 'cuda'
+
+# hyperparams
+ENCODER_EMBED_SIZE = 256
+ENCODER_HIDDEN_SIZE = 256
+DECODER_EMBED_SIZE = 256
+DECODER_HIDDEN_SIZE = 256
+IN_SEQ_LEN = 15
+OUT_SEQ_LEN = 15
+
+# Prepare text data
+download_and_extract_archive('https://www.manythings.org/anki/fra-eng.zip', './data/text')
+print('Data preprocessing..')
+with open('./data/text/fra.txt', 'r', encoding='utf-8') as f: # tab-separated file
+    docs_en, docs_fr, _ = zip(*[line.strip().split('\t') for line in f])
+
+# Tokenize and index
+text_tokenized_en = [clean_text(line, 'en').split() + ['<EOS>'] for line in docs_en]
+text_tokenized_fr = [clean_text(line, 'fr').split() + ['<EOS>'] for line in docs_fr]
+
+vocab_en = TextVocabulary(text_tokenized_en, min_freq=2, special_tokens=('<SOS>',))  #, special_tokens=('<SOS>', '<EOS>'))
+vocab_fr = TextVocabulary(text_tokenized_fr, min_freq=2, special_tokens=('<SOS>',))  #, special_tokens=('<SOS>', '<EOS>'))
+
+text_encoded_en = torch.tensor(vocab_en.encode_batch(text_tokenized_en, seq_length=IN_SEQ_LEN), dtype=torch.long)
+text_encoded_fr = torch.tensor(vocab_fr.encode_batch(text_tokenized_fr, seq_length=OUT_SEQ_LEN), dtype=torch.long)
+N = len(text_encoded_en)
+
+
+def diagnostics():
+    print(vocab_en)
+    print(vocab_fr)
+
+    len_en = [len(seq) for seq in text_tokenized_en]
+    len_fr = [len(seq) for seq in text_tokenized_fr]
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 3))
+    ax1.hist(len_en, bins=50, label='english', alpha=.5, color='blue')
+    ax2.hist(len_fr, bins=50, label='french', alpha=.5, color='orange')
+    ax1.axvline(IN_SEQ_LEN, label='max in sequence length', color='blue')
+    ax2.axvline(OUT_SEQ_LEN, label='max out sequence length', color='orange')
+    ax1.legend()
+    ax2.legend()
+    plt.suptitle('Length of sequences')
+    plt.tight_layout()
+    plt.show()
+
+    vocab_en.print_human(text_encoded_en[:5].numpy())
+    vocab_fr.print_human(text_encoded_fr[:5].numpy())
+diagnostics()
+
+
+
+# Model
+model = Seq2Seq(
+    encoder=Encoder(vocab_en.size, ENCODER_EMBED_SIZE, ENCODER_HIDDEN_SIZE, cell='lstm', n_layers=1, direction='backward', padding_idx=vocab_fr.to_idx['<PAD>']),
+    decoder=Decoder(vocab_fr.size, DECODER_EMBED_SIZE, DECODER_HIDDEN_SIZE, cell='lstm', n_layers=1, direction='forward', padding_idx=vocab_en.to_idx['<PAD>']),
+    sos_token=vocab_fr.to_idx['<SOS>'],
+)
+
+model.to(DEVICE)
+model.summary()
+optimizer = Adam(model.parameters(), lr=LEARN_RATE)
+
+
+def train(model, loader):
+    total_loss = total_acc = 0
+    pbar = trange(N, desc=f'Epoch (batch_size={BATCH_SIZE})')
+    for i, (X, Y, batch_frac) in enumerate(loader):
+        optimizer.zero_grad()
+        Z = model.forward(X, out_steps=OUT_SEQ_LEN, targets=Y)
+        cost = cross_entropy(Z, Y, logits=True)  # todo mask padding
+        cost.backward()
+        grad_clip_norm_(model.parameters(), 1.)
+        optimizer.step()
+
+        loss, acc = cost.item(), accuracy(Z.argmax(dim=-1), Y)  # todo: acc should ignore the padding
+        total_loss += loss * batch_frac
+        total_acc += acc * batch_frac
+
+        pbar.update(BATCH_SIZE)
+        pbar.set_postfix_str(f'loss={loss:.3f}, acc={acc:.3f}')
+
+    return total_loss, total_acc, pbar
+
+
+# Training
+for epoch in range(EPOCHS):
+    loader = batches(X=text_encoded_en, y=text_encoded_fr, batch_size=BATCH_SIZE, shuffle=True, device=DEVICE)  # todo: migrate to DataLoader
+    loss, acc, pbar = train(model, loader)
+    pbar.desc = f'Epoch {epoch+1}/{EPOCHS}'; pbar.set_postfix_str(f'{loss=:.3f}, {acc=:.3f}'); pbar.close()
+
+    with torch.no_grad():
+        n, offset = 5, N//2
+        X, Y = text_encoded_en[offset-n:offset], text_encoded_fr[offset-n:offset]
+        Z = model.forward(X.to(DEVICE), out_steps=OUT_SEQ_LEN)
+        Y_hat = Z.argmax(dim=-1).cpu()
+
+        print('')
+        for i in range(n):
+            print(f'{vocab_en.decode(X[i].numpy())} ({vocab_fr.decode(Y[i].numpy())}) -> {vocab_fr.decode(Y_hat[i].numpy())}')
--- src/preprocessing/text.py
+++ src/preprocessing/text.py
@@ -2,16 +2,20 @@ import re
 from itertools import combinations
 import numpy as np  # todo switch to torch
 from collections import Counter
+import contractions
+
 
 _patterns = (
-    (re.compile(r'[^a-zA-Z0-9_\-\s]'), ''),  # remove any special character
-    (re.compile(r'[\-_]'), ' '),             # convert dashes to spaces
-    (re.compile(r'\s+'), ' '),               # normalize spaces
+    # (re.compile(r'[^а-яА-Я0-9_\-\s]'), ''),  # remove any special character
+    # (re.compile(r'[\-_]'), ' '),             # convert dashes to spaces
+    (re.compile(r'([,.!?])'), r' \1 '),        # insert space between punctuations
+    (re.compile(r'\s+'), ' '),                 # normalize spaces
 )
 
-
-def clean_text(doc):
+def clean_text(doc, lang='en'):
     doc = doc.lower()
+    if lang == 'en':
+        doc = contractions.fix(doc)
     for pattern, repl in _patterns:
         doc = pattern.sub(repl, doc)
     return doc
@@ -38,19 +42,19 @@ def skip_grams(sequence, half_window=2, n=2, padding_token=0):
 
 class TextVocabulary:
 
-    def __init__(self, sequences, max_vocab_size=None, padding_token="<PAD>", unknown_token='<UNK>', special_tokens=()):
+    def __init__(self, sequences, max_vocab_size=None, min_freq=None, padding_token="<PAD>", unknown_token='<UNK>', special_tokens=()):
+        min_vocab_size = len(special_tokens) + 2  # including padding and unknown tokens
+        assert max_vocab_size is None or max_vocab_size > min_vocab_size, f'The vocabulary size cannot be less than {min_vocab_size}, because it must include the special tokens'
+
         words = [token for seq in sequences for token in seq]
+        limit = (max_vocab_size - min_vocab_size) if max_vocab_size is not None else None
 
-        dictionary = [(padding_token, 0), (unknown_token, 0)] + [(token, 0) for token in special_tokens]
+        selected = Counter(words).most_common(limit)
+        if min_freq is not None:
+            selected = [(word, freq) for word, freq in selected if freq >= min_freq]
 
-        if max_vocab_size is None:
-            dictionary += Counter(words).most_common()
-        else:
-            assert max_vocab_size > 2, 'The vocabulary size cannot be less than 2, because it must include the <padding> and <unknown> tokens'
-            selected = Counter(words).most_common(max_vocab_size - 2)
-            count_unknown = len(words) - sum(counts for word, counts in selected)
-            dictionary[1] = (unknown_token, count_unknown)
-            dictionary += selected
+        count_unknown = len(words) - sum(counts for word, counts in selected)
+        dictionary = [(padding_token, 0), (unknown_token, count_unknown)] + [(token, 0) for token in special_tokens] + selected
 
         self.size = len(dictionary)
         self.counts = np.array([counts for word, counts in dictionary])
@@ -74,8 +78,9 @@ class TextVocabulary:
             print(f'{seq} -> ', ' '.join([self.to_token[idx] for idx in seq if idx>0]))
 
     def __repr__(self):
-        tokens = '\n Top 10 tokens:\n'
-        for i in range(10):
+        n = min(self.size, 10)
+        tokens = f'\n Top {n} tokens:\n'
+        for i in range(n):
             tokens += f' {i}: {self.to_token[i]} ({self.counts[i]})\n'
 
         return f'TextVocabulary(size={self.size})' + tokens
