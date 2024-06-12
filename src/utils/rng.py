9305f47e28b8a06419d7f7e697d387e8c8e4840e
dichev <g.dichev@gmail.com>
2024-06-12 11:55:37 -0700

    Set cuda seed

--- src/utils/rng.py
+++ src/utils/rng.py
@@ -2,10 +2,16 @@ import numpy as np
 import torch
 import random
 
-def seed_global(seed: int):
+
+def seed_global(seed: int, deterministic=False):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    if deterministic:
+        torch.use_deterministic_algorithms(True)
+
 
 def pick_uniform(arr, n=1, exclude=None):
     if exclude is not None:
@@ -13,6 +19,7 @@ def pick_uniform(arr, n=1, exclude=None):
         mask[exclude] = False
         arr = arr[mask]
     return np.random.choice(arr, n)
+
 
 def sample_from(probs, n=1, exclude=None):
     if exclude is not None:
